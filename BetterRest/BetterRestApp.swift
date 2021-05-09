2623d3fcbc1cf3f850de471bf8af43f6830c854d
ivantrj <ivantrajanovski@gmail.com>
2021-05-09 15:56:38 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ivan Trajanovski on 2021. 05. 09..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
